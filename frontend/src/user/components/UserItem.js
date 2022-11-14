097d0991ac449871793755ee1dee8090b004f7c0
Matthew Akuhata <matthewakuhata@gmail.com>
2022-11-15 08:16:20 +1100

    Update: Add Card and Avatar to UserItem

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom'
+import { Avatar, Card } from '../../shared/components/UI'
 import './UserItem.css';
 
-const UserItem = ({name, image, placeCount}) => {
+const UserItem = ({id, name, image, placeCount}) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={image} alt={name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{name}</h2>
-          <h3>
-            {placeCount} {placeCount === 1 ? 'Place' : 'Places'}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <h3>
+              {placeCount} {placeCount === 1 ? 'Place' : 'Places'}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
