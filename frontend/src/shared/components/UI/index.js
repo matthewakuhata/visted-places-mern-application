27b4976be6adb56cb35a93e55b96aaffa83d7013
Matthew Akuhata <matthewakuhata@gmail.com>
2022-11-18 17:42:23 +1100

    Create: Loading Spinner

--- frontend/src/shared/components/UI/index.js
+++ frontend/src/shared/components/UI/index.js
@@ -1,5 +1,7 @@
-export { default as Avatar } from './Avatar/Avatar'
-export { default as Card } from './Card/Card'
-export { default as Backdrop } from './Backdrop/Backdrop'
-export { default as Modal } from './Modal/Modal'
-export { default as Map } from './Map/Map'
+export { default as Avatar } from "./Avatar/Avatar";
+export { default as Card } from "./Card/Card";
+export { default as Backdrop } from "./Backdrop/Backdrop";
+export { default as Modal } from "./Modal/Modal";
+export { default as Map } from "./Map/Map";
+export { default as LoadingSpinner } from "./LoadingSpinner/LoadingSpinner";
+export { default as ErrorModal } from "./ErrorModal/ErrorModal";
