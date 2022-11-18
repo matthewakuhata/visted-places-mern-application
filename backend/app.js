a57bdf36edab6cf4cb0f0e4bb2554f9ed9da1b31
Matthew Akuhata <matthewakuhata@gmail.com>
2022-11-18 17:42:40 +1100

    Update: Add CORS Headers

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,19 @@ const app = express();
 app.use(bodyParser.json());
 
 /**
+ *
+ */
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+    next();
+});
+/**
  * API Version 1
  */
 app.use("/api/v1/", api);
